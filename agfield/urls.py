50eb8f41a22d18ba1de0e095e448b514d0bc43f8
nfick <nfickett@gmail.com>
2018-08-01 11:31:26 -0500

    Start of renaming

--- agfield/urls.py
+++ agfield/urls.py
@@ -4,14 +4,14 @@ from django.urls import path
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import field.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^$', field.views.index, name='index'),
+    url(r'^field/', include('field.urls')),
     path('admin/', admin.site.urls),
 ]
--- hello/views.py
+++ hello/views.py
@@ -6,13 +6,8 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
+
     return render(request, 'index.html')
-    #r = requests.get('http://httpbin.org/status/418')
-    #print(r.text)
-    #return HttpResponse('<pre>' + r.text + '</pre>')
-    #times = int(os.environ.get('TIMES',3))
-    #return HttpResponse('Hello! ' * times)
 
 
 def db(request):
