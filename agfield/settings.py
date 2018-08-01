fa74935d73577220cb5597a9a3710a3d4f80dc2f
nfick <nfickett@gmail.com>
2018-08-01 17:01:15 -0500

    Added back stock html

--- agfield/settings.py
+++ agfield/settings.py
@@ -122,4 +122,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Local Settings
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+
 django_heroku.settings(locals())
