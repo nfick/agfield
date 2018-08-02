41eb0e40b652eeee3f530c6d4da62fc1e8c576cc
nfick <nfickett@gmail.com>
2018-08-02 14:47:25 -0500

    Fixed missing urls.py

--- field/urls.py
+++ field/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
--- field/views.py
+++ field/views.py
@@ -4,5 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-
     return render(request, 'index.html')
